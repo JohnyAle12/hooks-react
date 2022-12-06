72ed528e7e6a44c380912718b19a051ae3ba44bd
Johny Prieto <johnyprieto001@gmail.com>
2022-12-06 18:10:08 -0500

    init project to learn react hooks

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { CounterApp } from "./CounterApp/CounterApp"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks</h1>
+      <hr/>
+      <div className="container">
+        <div className='row'>
+          <CounterApp />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
